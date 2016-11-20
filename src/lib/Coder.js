19635ba82a44dfa086f5cbb1fe476333803c2a49
Gabriel Trabanco Llano <gtrabanco@fwok.org>
2016-11-20 22:26:47 +0000

    Modified the model for stats, not implemented yet.

--- src/lib/Coder.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-const index_1 = require("../../config/index");
-var Coder = require('base-encode');
-var enc = new Coder(index_1.options.alphabet);
-module.exports = enc;
--- src/models/access/model.ts
+++ src/models/access/model.ts
@@ -1,45 +1,52 @@
 import { Schema, Document, Model, SchemaTypes } from 'mongoose';
-import { options } from '../../../config';
 import { mongoose } from '../../services/database';
-import * as autoIncrement from 'mongoose-auto-increment';
 
 
 export interface IAccess extends Document {
-    userAgent:string,
-    ip:string,
-    encoding:string,
-    lang:string
+    _urlId: Object,
+    userAgent: string,
+    ip: string,
+    encoding :string,
+    lang: string,
+    referer: string,
+    created: Date
 }
 
 
-export interface IUrlModel {
-    getByStringId(str:string):Promise<IUrl>;
+export interface IAccessModel {
 }
 
-const UrlSchema = new Schema({
-    urlId: {
-        type: SchemaTypes.Number,
-        required: true,
-        index: {
-            unique: true
-        }
+const AccessSchema = new Schema({
+    _urlId: {
+        type: SchemaTypes.ObjectId,
+        ref: 'Url',
+        required: true
     },
-    url: {
+    userAgent: {
         type: SchemaTypes.String,
         required: true,
         index: {
             unique: true
         }
     },
+    ip: {
+        type: SchemaTypes.String,
+        required: true
+    },
+    encoding: {
+        type: SchemaTypes.String,
+        required: true
+    },
+    lang: {
+        type: SchemaTypes.String
+    },
+    referer: {
+        type: SchemaTypes.String
+    },
     created: {
         type: SchemaTypes.Date,
         default: Date.now,
         required: true
-    },
-    updated: {
-        type:SchemaTypes.Date,
-        default: Date.now,
-        required: false
     }
 }, {
     toObject: {
@@ -51,48 +58,7 @@ const UrlSchema = new Schema({
 });
 
 
-UrlSchema.statics.getByStringId = async (str:string):Promise<Document> => {
 
-    let id:number = parseInt(Coder.decode(str));
-    let url:Model<Document> = mongoose.model('Url');
+export type AccessModel = Model<IAccess> & IAccessModel & IAccess;
 
-    try {
-        let result = await url.findOne({urlId:id});
-
-        return result;
-    } catch (e) {
-        return Promise.reject(e);
-    }
-};
-
-UrlSchema.virtual('publicShortUrl').get(function () {
-    return options.publicURL + '/' + Coder.encode(parseInt(this.urlId));
-});
-
-UrlSchema.virtual('codedId').get(function () {
-    return Coder.encode(this.urlId);
-});
-
-
-UrlSchema.pre('save', (next:any) => {
-    this.updated = new Date();
-    return next();
-});
-
-
-//AutoIncrement Plugin
-//Initialize auto increment
-autoIncrement.initialize(mongoose.connection);
-
-//Auto Increment Schema
-UrlSchema.plugin(autoIncrement.plugin, {
-    model: 'Url',
-    field: 'urlId',
-    startAt: Math.pow(options.alphabet.length, parseInt(options.minLength)),
-    incrementBy: 1
-});
-
-
-export type UrlModel = Model<IUrl> & IUrlModel & IUrl;
-
-export const Url:UrlModel = <UrlModel>mongoose.model<IUrl>('Url', UrlSchema);
+export const Access:AccessModel = <AccessModel>mongoose.model<IAccess>('Access', AccessSchema);
